54c78227aa4e288b622dc0a931a755889b4ab0a9
Clofender <danielsfn1@gmail.com>
2025-08-12 00:33:36 -0300

    feat(clientes): implementa serviço de criação de clientes

--- backend/src/clientes/clientes.service.ts
+++ backend/src/clientes/clientes.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateClienteDto } from './dto/create-cliente.dto';
-import { UpdateClienteDto } from './dto/update-cliente.dto';
+// import { UpdateClienteDto } from './dto/update-cliente.dto';
+import { Cliente } from './entities/cliente.entity';
 
 @Injectable()
 export class ClientesService {
-  create(createClienteDto: CreateClienteDto) {
-    return 'This action adds a new cliente';
+  constructor(
+    @InjectRepository(Cliente)
+    private readonly clientesRepository: Repository<Cliente>,
+  ) {}
+
+  create(createClienteDto: CreateClienteDto): Promise<Cliente> {
+    const cliente = this.clientesRepository.create(createClienteDto);
+    return this.clientesRepository.save(cliente);
   }
 
   findAll() {
@@ -16,7 +25,7 @@ export class ClientesService {
     return `This action returns a #${id} cliente`;
   }
 
-  update(id: number, updateClienteDto: UpdateClienteDto) {
+  update(id: number) {
     return `This action updates a #${id} cliente`;
   }
 
